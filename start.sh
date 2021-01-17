5c88ee4e6ab8c2f1ae05cb58920300ac5a681da5
Apsd1109 <Apsd1109@hotmail.ca>
2021-01-16 23:06:46 -0500

    enhance scripts

--- start.sh
+++ start.sh
@@ -1,8 +1,52 @@
 #!/bin/bash
-WATID=YOUR_WATID
-SERVER_NUM=EITHER_1_OR_2
-PROJ_DIR=CURRENT_DIRECTOR_NAME
+
+help(){
+	echo
+	echo "FORMATTING:"
+	echo "./setup.sh <WATID> <SERVER NUMBER (1 or 2)> <DIRECTORY NAME>"
+	echo "DIRECTORY NAME IS WHERE YOUR FILES WILL BE COPIED TO ON THE ECE SERVERS. THE PATH WILL BE:"	
+	echo "/home/<WATID>/projects/<DIRECTORY NAME>"
+	echo "EXITING..."
+	echo
+	exit -1	
+}
+
+if [ -z "$1" ]
+	then
+		echo "NO WATID SPECIFIED (ie. k5kumara)."
+		help
+	else
+		WATID=$1
+fi
+
+if [ -z "$2" ]
+	then
+		echo "NO SERVER NUMBER SPECIFIED (ie. 1 or 2)."
+		help
+	else
+		if [ "$2" == "1" ] || [ "$2" == "2" ]
+			then
+				SERVER_NUM=$2
+			else
+				echo SERVER NUMBER MUST BE 1 OR 2
+				help
+		fi
+fi
+
+if [ -z "$3" ]
+	then
+		echo "NO DIRECTORY NAME SPECIFIED (ie. "ECE250-p1")."
+		help
+	else
+		PROJ_DIR=$3
+fi
 
 ssh $WATID@eceubuntu$SERVER_NUM.uwaterloo.ca  "mkdir /home/$WATID/projects;mkdir /home/$WATID/projects/$PROJ_DIR;exit"
 scp -r ../$PROJ_DIR $WATID@eceubuntu1.uwaterloo.ca:/home/$WATID/projects
 ssh $WATID@eceubuntu$SERVER_NUM.uwaterloo.ca  "g++ /home/$WATID/projects/$PROJ_DIR/*.cpp -o a.out;echo 'running program...';./a.out"
+
+echo "PROJECT COPIED TO:"
+echo "/home/$WATID/projects/$PROJ_DIR"
+echo "SSH INTO THE SERVER:"
+echo "ssh -X $WATID@eceubuntu$SERVER_NUM.uwaterloo.ca"
+
