3208a416b94db57e2f686fb65992ac3a9d17584a
Deandre Baker <49622234+deandrebaker@users.noreply.github.com>
2021-01-16 22:06:20 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,5 +2,5 @@
 WATID=YOUR_WAT_ID
 SERVER_NUM=EITHER_1_OR_2
 
-ssh-keygen
-ssh-copy-id -i ~/.ssh/id_rsa.pub $WATID@eceubuntu$SERVER_NUM.uwaterloo.ca
+ssh-keygen -t rsa -N "" -f ece_key
+ssh-copy-id -i ece_key.pub $WATID@eceubuntu$SERVER_NUM.uwaterloo.ca
