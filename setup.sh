8e98560dcf111a2ab520b5420ccdcae934e07203
Apsd1109 <46359439+Apsd1109@users.noreply.github.com>
2021-01-17 11:36:59 -0500

    enhance scripts (#4)
    * enhance scripts
    
    * revise start.sh
    
    * revise setup.sh
    
    * check for failed compilation
    
    * revert back to original + add ssh with keys

--- setup.sh
+++ setup.sh
@@ -1,6 +1,38 @@
 #!/bin/bash
-WATID=YOUR_WAT_ID
-SERVER_NUM=EITHER_1_OR_2
+
+help(){
+	echo
+	echo "FORMATTING:"
+	echo "./setup.sh <WATID> <SERVER NUMBER (1 or 2)>"	
+	echo "EXITING..."
+	echo
+	exit -1	
+}
+
+if [ -z "$1" ]
+	then
+		echo "NO WATID SPECIFIED (ie. k5kumara)."
+		help
+	else
+		WATID=$1
+fi
+
+if [ -z "$2" ]
+	then
+		echo "NO SERVER NUMBER SPECIFIED (ie. 1 or 2)."
+		help
+	else
+		if [ "$2" == "1" ] || [ "$2" == "2" ]
+			then
+				SERVER_NUM=$2
+			else
+				echo SERVER NUMBER MUST BE 1 OR 2
+				help
+		fi
+fi
 
 ssh-keygen -t rsa -N "" -f ece_key
 ssh-copy-id -i ece_key.pub $WATID@eceubuntu$SERVER_NUM.uwaterloo.ca
+
+echo "SSH KEY GENERATED FOR eceubuntu$SERVER_NUM.uwaterloo.ca"
+
