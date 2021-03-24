4ebd8f72819bfd95dfda8d921f8339f639820d4e
ryanz34 <ryanzhang0112@gmail.com>
2021-03-24 00:05:18 -0400

    P2 testcase generator (#19)

---
+++ p2/Test Case Generator/README.md
@@ -0,0 +1,40 @@
+# Test Case Generator for P2
+This is a automatic test case generator for ECE 250 Project 2. The python file 
+will automatically generate test cases and test your code. Please compile your 
+project on ECEUbuntu
+
+## READ BEFORE RUNNING
+
+Note: This generator attempts to generate long test files in the hopes that it will cover 
+all possible inputs. It might not test all possibilities and does not replace carefully made
+test cases by real people.
+
+
+## Running
+To run this code first copy it to your project folder on ECEUbuntu and compile your project.
+
+After, run
+```bash
+python3 TestCaseGenerator.py
+```
+
+The python file will test and generate test cases in a new folder called testcases and 
+multiple different files.
+
+test#.in - input of testcase  
+test#.out - expected output of testcase  
+\#.o - your program's output  
+test#.failed - differences between your output and expected output
+
+where # is the test case number.
+
+The program will not delete the previous testcases. To run this generator again,
+please first run 
+
+```bash
+rm -r testcases
+```
+
+in your project directory to delete all previous testcases.
+
+If there are any issues, please contact ryanz34#0442 on discord.
---
+++ p2/Test Case Generator/TestCaseGenerator.py
@@ -0,0 +1,174 @@
+from random import *
+import os
+import shutil
+import subprocess
+
+NUM_START = 1
+TEST_CASES = 300
+LENGTH = 2000
+
+TEST_DIR = "testcases"
+
+words = ['a', 'aa', 'aaa', 'aaaa', 'aaaaaa','aaaaaaaaa', 'aaaaaaaaaaa', 'ability', 'able', 'about', 'above', 'accept', 'according', 'account', 'across', 'act', 'action', 'activity', 'actually', 'add', 'address', 'administration', 'admit', 'adult', 'affect', 'after', 'again', 'against', 'age', 'agency', 'agent', 'ago', 'agree', 'agreement', 'ahead', 'air', 'all', 'allow', 'almost', 'alone', 'along', 'already', 'also', 'although', 'always', 'American', 'among', 'amount', 'analysis', 'and', 'animal', 'another', 'answer', 'any', 'anyone', 'anything', 'appear', 'apply', 'approach', 'area', 'argue', 'arm', 'around', 'arrive', 'art', 'article', 'artist', 'as', 'ask', 'assume', 'at', 'attack', 'attention', 'attorney', 'audience', 'author', 'authority', 'available', 'avoid', 'away', 'baby', 'back', 'bad', 'bag', 'ball', 'bank', 'bar', 'base', 'be', 'beat', 'beautiful', 'because', 'become', 'bed', 'before', 'begin', 'behavior', 'behind', 'believe', 'benefit', 'best', 'better', 'between', 'beyond', 'big', 'bill', 'billion', 'bit', 'black', 'blood', 'blue', 'board', 'body', 'book', 'born', 'both', 'box', 'boy', 'break', 'bring', 'brother', 'budget', 'build', 'building', 'business', 'but', 'buy', 'by', 'call', 'camera', 'campaign', 'can', 'cancer', 'candidate', 'capital', 'car', 'card', 'care', 'career', 'carry', 'case', 'catch', 'cause', 'cell', 'center', 'central', 'century', 'certain', 'certainly', 'chair', 'challenge', 'chance', 'change', 'character', 'charge', 'check', 'child', 'choice', 'choose', 'church', 'citizen', 'city', 'civil', 'claim', 'class', 'clear', 'clearly', 'close', 'coach', 'cold', 'collection', 'college', 'color', 'come', 'commercial', 'common', 'community', 'company', 'compare', 'computer', 'concern', 'condition', 'conference', 'Congress', 'consider', 'consumer', 'contain', 'continue', 'control', 'cost', 'could', 'country', 'couple', 'course', 'court', 'cover', 'create', 'crime', 'cultural', 'culture', 'cup', 'current', 'customer', 'cut', 'dark', 'data', 'daughter', 'day', 'dead', 'deal', 'death', 'debate', 'decade', 'decide', 'decision', 'deep', 'defense', 'degree', 'Democrat', 'democratic', 'describe', 'design', 'despite', 'detail', 'determine', 'develop', 'development', 'die', 'difference', 'different', 'difficult', 'dinner', 'direction', 'director', 'discover', 'discuss', 'discussion', 'disease', 'do', 'doctor', 'dog', 'door', 'down', 'draw', 'dream', 'drive', 'drop', 'drug', 'during', 'each', 'early', 'east', 'easy', 'eat', 'economic', 'economy', 'edge', 'education', 'effect', 'effort', 'eight', 'either', 'election', 'else', 'employee', 'end', 'energy', 'enjoy', 'enough', 'enter', 'entire', 'environment', 'environmental', 'especially', 'establish', 'even', 'evening', 'event', 'ever', 'every', 'everybody', 'everyone', 'everything', 'evidence', 'exactly', 'example', 'executive', 'exist', 'expect', 'experience', 'expert', 'explain', 'eye', 'face', 'fact', 'factor', 'fail', 'fall', 'family', 'far', 'fast', 'father', 'fear', 'federal', 'feel', 'feeling', 'few', 'field', 'fight', 'figure', 'fill', 'film', 'final', 'finally', 'financial', 'find', 'fine', 'finger', 'finish', 'fire', 'firm', 'first', 'fish', 'five', 'floor', 'fly', 'focus', 'follow', 'food', 'foot', 'for', 'force', 'foreign', 'forget', 'form', 'former', 'forward', 'four', 'free', 'friend', 'from', 'front', 'full', 'fund', 'future', 'game', 'garden', 'gas', 'general', 'generation', 'get', 'girl', 'give', 'glass', 'go', 'goal', 'good', 'government', 'great', 'green', 'ground', 'group', 'grow', 'growth', 'guess', 'gun', 'guy', 'hair', 'half', 'hand', 'hang', 'happen', 'happy', 'hard', 'have', 'he', 'head', 'health', 'hear', 'heart', 'heat', 'heavy', 'help', 'her', 'here', 'herself', 'high', 'him', 'himself', 'his', 'history', 'hit', 'hold', 'home', 'hope', 'hospital', 'hot', 'hotel', 'hour', 'house', 'how', 'however', 'huge', 'human', 'hundred', 'husband', 'I', 'idea', 'identify', 'if', 'image', 'imagine', 'impact', 'important', 'improve', 'in', 'include', 'including', 'increase', 'indeed', 'indicate', 'individual', 'industry', 'information', 'inside', 'instead', 'institution', 'interest', 'interesting', 'international', 'interview', 'into', 'investment', 'involve', 'issue', 'it', 'item', 'its', 'itself', 'job', 'join', 'just', 'keep', 'key', 'kid', 'kill', 'kind', 'kitchen', 'know', 'knowledge', 'land', 'language', 'large', 'last', 'late', 'later', 'laugh', 'law', 'lawyer', 'lay', 'lead', 'leader', 'learn', 'least', 'leave', 'left', 'leg', 'legal', 'less', 'let', 'letter', 'level', 'lie', 'life', 'light', 'like', 'likely', 'line', 'list', 'listen', 'little', 'live', 'local', 'long', 'look', 'lose', 'loss', 'lot', 'love', 'low', 'machine', 'magazine', 'main', 'maintain', 'major', 'majority', 'make', 'man', 'manage', 'management', 'manager', 'many', 'market', 'marriage', 'material', 'matter', 'may', 'maybe', 'me', 'mean', 'measure', 'media', 'medical', 'meet', 'meeting', 'member', 'memory', 'mention', 'message', 'method', 'middle', 'might', 'military', 'million', 'mind', 'minute', 'miss', 'mission', 'model', 'modern', 'moment', 'money', 'month', 'more', 'morning', 'most', 'mother', 'mouth', 'move', 'movement', 'movie', 'Mr', 'Mrs', 'much', 'music', 'must', 'my', 'myself', 'name', 'nation', 'national', 'natural', 'nature', 'near', 'nearly', 'necessary', 'need', 'network', 'never', 'new', 'news', 'newspaper', 'next', 'nice', 'night', 'no', 'none', 'nor', 'north', 'not', 'note', 'nothing', 'notice', 'now', "nt", 'number', 'occur', 'of', 'off', 'offer', 'office', 'officer', 'official', 'often', 'oh', 'oil', 'ok', 'old', 'on', 'once', 'one', 'only', 'onto', 'open', 'operation', 'opportunity', 'option', 'or', 'order', 'organization', 'other', 'others', 'our', 'out', 'outside', 'over', 'own', 'owner', 'page', 'pain', 'painting', 'paper', 'parent', 'part', 'participant', 'particular', 'particularly', 'partner', 'party', 'pass', 'past', 'patient', 'pattern', 'pay', 'peace', 'people', 'per', 'perform', 'performance', 'perhaps', 'period', 'person', 'personal', 'phone', 'physical', 'pick', 'picture', 'piece', 'place', 'plan', 'plant', 'play', 'player', 'PM', 'point', 'police', 'policy', 'political', 'politics', 'poor', 'popular', 'population', 'position', 'positive', 'possible', 'power', 'practice', 'prepare', 'present', 'president', 'pressure', 'pretty', 'prevent', 'price', 'private', 'probably', 'problem', 'process', 'produce', 'product', 'production', 'professional', 'professor', 'program', 'project', 'property', 'protect', 'prove', 'provide', 'public', 'pull', 'purpose', 'push', 'put', 'quality', 'question', 'quickly', 'quite', 'race', 'radio', 'raise', 'range', 'rate', 'rather', 'reach', 'read', 'ready', 'real', 'reality', 'realize', 'really', 'reason', 'receive', 'recent', 'recently', 'recognize', 'record', 'red', 'reduce', 'reflect', 'region', 'relate', 'relationship', 'religious', 'remain', 'remember', 'remove', 'report', 'represent', 'Republican', 'require', 'research', 'resource', 'respond', 'response', 'responsibility', 'rest', 'result', 'return', 'reveal', 'rich', 'right', 'rise', 'risk', 'road', 'rock', 'role', 'room', 'rule', 'run', 'safe', 'same', 'save', 'say', 'scene', 'school', 'science', 'scientist', 'score', 'sea', 'season', 'seat', 'second', 'section', 'security', 'see', 'seek', 'seem', 'sell', 'send', 'senior', 'sense', 'series', 'serious', 'serve', 'service', 'set', 'seven', 'several', 'sex', 'sexual', 'shake', 'share', 'she', 'shoot', 'short', 'shot', 'should', 'shoulder', 'show', 'side', 'sign', 'significant', 'similar', 'simple', 'simply', 'since', 'sing', 'single', 'sister', 'sit', 'site', 'situation', 'six', 'size', 'skill', 'skin', 'small', 'smile', 'so', 'social', 'society', 'soldier', 'some', 'somebody', 'someone', 'something', 'sometimes', 'son', 'song', 'soon', 'sort', 'sound', 'source', 'south', 'southern', 'space', 'speak', 'special', 'specific', 'speech', 'spend', 'sport', 'spring', 'staff', 'stage', 'stand', 'standard', 'star', 'start', 'state', 'statement', 'station', 'stay', 'step', 'still', 'stock', 'stop', 'store', 'story', 'strategy', 'street', 'strong', 'structure', 'student', 'study', 'stuff', 'style', 'subject', 'success', 'successful', 'such', 'suddenly', 'suffer', 'suggest', 'summer', 'support', 'sure', 'surface', 'system', 'table', 'take', 'talk', 'task', 'tax', 'teach', 'teacher', 'team', 'technology', 'television', 'tell', 'ten', 'tend', 'term', 'test', 'than', 'thank', 'that', 'the', 'their', 'them', 'themselves', 'then', 'theory', 'there', 'these', 'they', 'thing', 'think', 'third', 'this', 'those', 'though', 'thought', 'thousand', 'threat', 'three', 'through', 'throughout', 'throw', 'thus', 'time', 'to', 'today', 'together', 'tonight', 'too', 'top', 'total', 'tough', 'toward', 'town', 'trade', 'traditional', 'training', 'travel', 'treat', 'treatment', 'tree', 'trial', 'trip', 'trouble', 'true', 'truth', 'try', 'turn', 'TV', 'two', 'type', 'under', 'understand', 'unit', 'until', 'up', 'upon', 'us', 'use', 'usually', 'value', 'various', 'very', 'victim', 'view', 'violence', 'visit', 'voice', 'vote', 'wait', 'walk', 'wall', 'want', 'war', 'watch', 'water', 'way', 'we', 'weapon', 'wear', 'week', 'weight', 'well', 'west', 'western', 'what', 'whatever', 'when', 'where', 'whether', 'which', 'while', 'white', 'who', 'whole', 'whom', 'whose', 'why', 'wide', 'wife', 'will', 'win', 'wind', 'window', 'wish', 'with', 'within', 'without', 'woman', 'wonder', 'word', 'work', 'worker', 'world', 'worry', 'would', 'write', 'writer', 'wrong', 'yard', 'yeah', 'year', 'yes', 'yet', 'you', 'young', 'your', 'yourself']
+words = [x.lower() for x in words]
+illegal = list(" ;:\'\",<.>/?[{]}\\|-_=+)(*&^%$#@!`~)}]ABCDEFGHIJKLMNOPQRSTUVWXYZ")
+alpha = list("abcdefghijklmnopqrstuvwxyz")
+
+
+def illegal_gen():
+    w = []
+    a = choice(words)
+    if len(a) != 1:
+        w.append(a[:randint(0, len(a)-1)])
+    else:
+        w.append(a)
+    w.append(choice(illegal))
+
+    for i in range(randint(1, 100)):
+        w.append(choice(choice([alpha, illegal])))
+
+    return "".join(w)
+
+
+if os.path.exists(TEST_DIR):
+    print("file/folder with name " + TEST_DIR + " detected. Please remove")
+    raise SystemExit
+
+os.mkdir(TEST_DIR)
+
+print("Test dir made")
+print("Preparing Test Cases")
+
+for i in range(NUM_START, NUM_START+TEST_CASES):
+    inp = open(TEST_DIR + "/test" + str(i) + ".in", "w")
+    out = open(TEST_DIR + "/test" + str(i) + ".out", "w")
+
+    current_words = set()
+
+    for l in range(LENGTH):
+        cmd = randint(0, 10)
+
+        if cmd in [0, 1, 2, 3]:
+            if randint(0, 2) <= 1:
+                k = randint(0, 2)
+                if k <= 1 or len(current_words) == 0:
+                    w = choice(words)
+                else:
+                    w = choice(list(current_words))
+
+                if w in current_words:
+                    out.write("failure\n")
+                else:
+                    out.write("success\n")
+                    current_words.add(w)
+
+            else:
+                w = illegal_gen()
+                out.write("illegal argument\n")
+
+            inp.write("i {0}\n".format(w))
+
+        elif cmd in [4, 5]:
+            if randint(0, 2) <= 1:
+                k = randint(0, 2)
+                if k == 0 or len(current_words) == 0:
+                    w = choice(words)
+                elif k == 1:
+                    w = choice(list(current_words))
+                else:
+                    w = choice(list(current_words))
+                    if len(w) != 1:
+                        w = w[:randint(1, len(w)-1)]
+
+
+                if w in current_words:
+                    out.write("success\n")
+                    current_words.remove(w)
+                else:
+                    out.write("failure\n")
+
+            else:
+                w = illegal_gen()
+                out.write("illegal argument\n")
+
+            inp.write("e {0}\n".format(w))
+
+        elif cmd in [6, 7]:
+            if randint(0, 2) <= 1:
+                w = choice(words)
+
+                if w in current_words:
+                    out.write("found " + w + "\n")
+                else:
+                    out.write("not found\n")
+
+            else:
+                w = illegal_gen()
+                out.write("illegal argument\n")
+
+            inp.write("s {0}\n".format(w))
+
+        elif cmd in [8, 9]:
+            if (randint(0, 1) == 0 or len(set(words) - current_words) == 0) and len(current_words) != 0:
+                w = choice(list(current_words))
+                if len(w) != 1:
+                    w = w[:randint(1, len(w)-1)]
+
+                inp.write("autocomplete {0}*\n".format(w))
+
+            else:
+                w = choice(list(set(words) - current_words))
+                if len(w) != 1:
+                    w = w[:randint(1, len(w)-1)]
+
+                for _ in range(randint(0, 10)):
+                    w += choice(alpha)
+
+                inp.write("autocomplete {0}*\n".format(w))
+
+            found_words = []
+
+            for j in current_words:
+                if len(j) >= len(w) and j[:len(w)] == w:
+                    found_words.append(j)
+
+            if len(found_words) != 0:
+                out.write(" ".join(sorted(found_words))+"\n")
+
+        elif cmd in [10]:
+            inp.write("clear\n")
+            current_words = set()
+            out.write("success\n")
+
+        inp.write("size\n")
+        out.write("number of words is {0}\n".format(len(current_words)))
+
+        inp.write("empty\n")
+        out.write("empty {0}\n".format(1 if len(current_words) == 0 else 0))
+
+        inp.write("print\n")
+        if len(current_words) != 0:
+            out.write(" ".join(sorted(current_words))+"\n")
+
+    inp.write("exit\n")
+    inp.close()
+    out.close()
+
+    print("Running test case " + str(i))
+    output = subprocess.check_output(
+        "./triedriver < {0}/{1} > {0}/{2}".format(TEST_DIR, "test" + str(i) + ".in", str(i) + ".o"),
+        stderr=subprocess.STDOUT, timeout=10, shell=True)
+
+    p = subprocess.run(
+        "diff {0}/{1} {0}/{2}".format(TEST_DIR, "test" + str(i) + ".out", str(i) + ".o"),
+        stderr=subprocess.PIPE, stdout=subprocess.PIPE, timeout=10, shell=True, check=False)
+
+    if p.returncode == 0:
+        print("success")
+    else:
+        print("failed")
+        with open(TEST_DIR + "/" + "test" + str(i) + ".failed", "wb") as f:
+            f.write(p.stdout)
+
+        print("wrote differences to {0}/{1}".format(TEST_DIR, "test"+str(i)+".failed"))
+
